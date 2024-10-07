769aaa17cb5bcb8dc394c7a41085ee9e40640995
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-07 16:00:53 +0200

    create a function in iniziali script.js

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,9 +4,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function findArrNames(array) {
+    for (i = 0; i < array.length; i++) {
+        const element = array[i];
+        //console.log(element);
+        const firstLetter = element[0];
+        //console.log(firstLetter);
+        const firstLetterArr = [];
+        firstLetterArr.push(firstLetter);
+        return firstLetterArr
+        
+        
+    }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(findArrNames(names));
 
 
 
